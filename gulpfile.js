1b847521a43cc6dcf701e38858b51bc4ee624556
fateme ebrahimzadeh <fateme.ebrahimzadeh20@gmail.com>
2023-02-13 12:53:19 +0330

    add gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,22 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-}
+const { src, dest } = require('gulp');
+const concat = require('gulp-concat');
 
-exports.default = defaultTask
+const cssBundle = () =>
+    src([
+        'assets/styles/about-hotel.css',
+        'assets/styles/aside.css',
+        'assets/styles/features.css',
+        'assets/styles/footer.css',
+        'assets/styles/gallery.css',
+        'assets/styles/global.css',
+        'assets/styles/header.css',
+        'assets/styles/main.css',
+        'assets/styles/modal.css',
+        'assets/styles/other-places.css',
+        'assets/styles/rooms.css',
+        'assets/styles/ruls.css'
+    ])
+        .pipe(concat('styles.css'))
+        .pipe(dest('dist/css'));
+
+exports.cssBundle = cssBundle;
